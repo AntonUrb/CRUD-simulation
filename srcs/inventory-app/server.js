c379054d00c2bddfb07f8868946f382f082a53e6
karlutska <karlutska@NAME>
2024-06-18 23:08:45 +0300

    messing with config

--- srcs/inventory-app/server.js
+++ srcs/inventory-app/server.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const {Sequelize} = require("sequelize")
+const process = require("process");
+const env = process.env.NODE_ENV || "development"
+const config = require(__dirname + '/app/config/config.json')[env]
 const app = express();
 const port = 5000
 
-const routes = require("./app/routes/Routes")
-const sequelize = new Sequelize('postgres://postgres:abcd1234@localhost:5432/postgres')
+const routes = require("./app/routes/Routes");
+const sequelize = new Sequelize (`${config.database}://${config.database}:${config.password}@${config.host}:${config.port}/${config.database}`)
+//const sequelize = new Sequelize('postgres://postgres:abcd1234@localhost:5432/postgres')
 
 app.use("/api/movies", routes)
 
